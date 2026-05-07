232fa0cf135be9e08233f47e4a490e46462a312d
carplive2018-arch <carplive2018@gmail.com>
2026-05-08 05:01:49 +0900

    Update router.py

--- app/api/router.py
+++ app/api/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.lineup import router as lineup_router
+
 from app.api.public import router as public_router
 
 api_router = APIRouter()
 api_router.include_router(public_router)
-api_router.include_router(lineup_router, prefix="/api")
+
